afe3e518cd25117db3039ce97ef14660adeb064e
Hellowor1d <1034597776@qq.com>
2015-07-22 15:09:52 +0800

    增加倒计时功能
    1.添加了倒计时功能，以及计时结束游戏停止（后续，大家可以添加游戏结束时的弹出画面，显示得分，或者添加重新开始游戏的按钮，都有现成函数，可以直接调用）
    2.更改了兔子移动速度为300，操纵流畅一些了
    3.游戏不明原因出现“瀑布元宝”的BUG，大家可以多留意一下发生的条件，我们慢慢排查
    4.下一步考虑进行移动端兼容，自适应移动端屏幕大小，键盘操控或去除，或添加鼠标操控以及实现触控兼容

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ var main_state = {
         money.enableBody=true;
         //实现每秒掉落元宝一枚
         this.game.time.events.loop(1000,this.addMoney,this);
+        //l利用定时器调用倒计时函数，实现每秒减计数
+        this.game.time.events.loop(1000, this.subTime, this);
         //添加键盘监视对象
         cursors = game.input.keyboard.createCursorKeys();
         //添加分数显示
@@ -22,17 +24,24 @@ var main_state = {
         var style={font:'30px Arial',fill:'#ffffff'};
         this.scoreText=game.add.text(10,10,'分数：0',style);
         getmoney = game.add.audio('getmoney');
+        //添加倒计时功能
+        this.time = 30;
+        var style = {
+            font: '30px Arial',
+            fill: '#ffffff'
+        };
+        this.timeText = game.add.text(350, 10, '时间：30', style);
     },
 //游戏每一帧会调用update函数，每秒60帧，实现监听、碰撞检测等
     update: function() {
        cat.body.velocity.setTo(0, 0);
         if (cursors.left.isDown)
         {
-            cat.body.velocity.x = -200;
+            cat.body.velocity.x = -300;
         }
         else if (cursors.right.isDown)
         {
-            cat.body.velocity.x = 200;
+            cat.body.velocity.x = 300;
         }
         //碰撞检测
         game.physics.overlap(cat,money,this.collectMoney,null,this);
@@ -44,7 +53,16 @@ var main_state = {
         this.scoreText.content='分数：'+this.score;
         getmoney.play();
     },
-    
+    //倒计时函数
+    subTime: function() {
+        if (this.time > 0) {
+            this.time -= 1;
+            this.timeText.content = '时间：' + this.time;
+        } else {
+            // 计时停止时，让游戏也结束
+            this.game.state.end('main');
+        }
+    },
     addMoney:function(){
         //元宝的位置在y=80的位置随机生成
         var i=Math.floor(Math.random()*5+1);
