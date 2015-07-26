a99d8c0a3a9da7cc829873bf21b162985aafc975
PrototypeWB <544039031@qq.com>
2015-07-26 12:38:15 +0800

    适配成功

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game(640, 960, Phaser.AUTO, 'game');
 var main_state = {
 //加载游戏图片资源
     preload: function() {
@@ -12,12 +12,18 @@ var main_state = {
     },
 //创建对象
     create: function() { 
+        // 设置比例选项
+		this.input.maxPointers = 1;
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.scale.setScreenSize(true);
         field=game.add.sprite(0,0,'field');
-        cat=game.add.sprite(100,450,'cat');
+        cat=game.add.sprite(100,800,'cat');
         money= game.add.group();
         money.enableBody=true;
-        //实现每秒掉落元宝一枚
-        this.game.time.events.loop(1000,this.addMoney,this);
+        //实现每600毫秒掉落元宝一枚
+        this.game.time.events.loop(600,this.addMoney,this);
         //l利用定时器调用倒计时函数，实现每秒减计数
         this.game.time.events.loop(1000, this.subTime, this);
         //添加键盘监视对象
@@ -25,7 +31,10 @@ var main_state = {
         //添加分数显示
         this.score=0;
         var style={font:'30px Arial',fill:'#ffffff'};
-        this.scoreText=game.add.text(10,10,'分数：0',style);
+        this.scoreText=this.add.text(10,10,'分数：0',style);
+   
+        
+        
         getmoney = game.add.audio('getmoney');
         //添加倒计时功能
         this.time = 30;
@@ -40,7 +49,8 @@ var main_state = {
     },
 //游戏每一帧会调用update函数，每秒60帧，实现监听、碰撞检测等
     update: function() {
-       cat.body.velocity.setTo(0, 0);
+       game.physics.enable(cat, Phaser.Physics.ARCADE);
+       cat.body.velocity.x=0;
         if (cursors.left.isDown)
         {
             cat.body.velocity.x = -300;
@@ -56,20 +66,20 @@ var main_state = {
             right.kill();
         }
         //碰撞检测
-        game.physics.overlap(cat,money,this.collectMoney,null,this);
+        game.physics.arcade.overlap(cat,money,this.collectMoney,null,this);
     } ,
     
    collectMoney: function(cat,money) {
         money.kill();
         this.score+=1;
-        this.scoreText.content='分数：'+this.score;
+        this.scoreText.setText('分数：'+this.score);
         getmoney.play();
     },
     //倒计时函数
     subTime: function() {
         if (this.time > 0) {
             this.time -= 1;
-            this.timeText.content = '时间：' + this.time;
+            this.timeText.setText('时间：' + this.time);
         } else {
             // 计时停止时，让游戏也结束
             this.game.state.end('main');
@@ -80,7 +90,7 @@ var main_state = {
         //元宝的位置在y=80的位置随机生成
         var i=Math.floor(Math.random()*5+1);
         onemoney=money.create(i*80,80,'money');
-        onemoney.body.gravity.y=300;         
+        onemoney.body.gravity.y=500;         
     }
 }
 
