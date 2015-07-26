95fee02880bf2d7847a869071559853a49e8c2f8
SoftSoftCat <love@bug.com>
2015-07-26 14:05:51 +0800

    限制移动范围
    通过判定将移动范围控制在视野内。

--- main.js
+++ main.js
@@ -55,6 +55,9 @@ var main_state = {
             cat.body.velocity.x = 300;
         }
         //碰撞检测
+        if (cat.body.sprite.x<0) {cat.body.velocity.x+=300;}
+        else if(cat.body.sprite.x>560) {cat.body.velocity.x-=300;}
+        //限制活动范围
         game.physics.arcade.overlap(cat,money,this.collectMoney,null,this);
     } ,
     
